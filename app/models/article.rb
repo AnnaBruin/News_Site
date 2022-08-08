a01cd5adb114d16f12fc999b10d94ec0af50724c
AnnaBruin <91970164+AnnaBruin@users.noreply.github.com>
2022-08-08 13:48:28 +0300

    Added tags for articles and navigation through them

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,19 @@ class Article < ApplicationRecord
 
     mount_uploader :image, ImageUploader
 
+    has_many :taggings, dependent: :destroy
+    has_many :tags, through: :taggings
+
     validates :title, :summary, presence: true
     validates :body, length: { minimum: 10 }
+
+    def all_tags
+        self.tags.map(&:name).join(', ')
+    end
+  
+    def all_tags=(names)
+        self.tags = names.split(',').map do |name|
+            Tag.where(name: name.strip).first_or_create!
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_06_125121) do
+ActiveRecord::Schema.define(version: 2022_08_07_131937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,21 @@ ActiveRecord::Schema.define(version: 2022_08_06_125121) do
     t.string "image"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -83,4 +98,6 @@ ActiveRecord::Schema.define(version: 2022_08_06_125121) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "taggings", "articles"
+  add_foreign_key "taggings", "tags"
 end
